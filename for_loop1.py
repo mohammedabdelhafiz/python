c0fc8403fae2dd38fb8925f9ee38a1a87ffe4c32
Mohammed Abdelhafiz <114105942+mohammedabdelhafiz@users.noreply.github.com>
2022-11-10 21:55:47 +0200

    Add files via upload

---
+++ for_loop1.py
@@ -0,0 +1,31 @@
+for x in range(0, 151, 1):
+    print (x)
+
+for y in range (1, 1000 , 1):
+    if y%5==0:
+         print(y)
+
+for z in range (1, 101 , 1):
+    if z%5==0:
+         print("Coding")
+    
+    elif z%10==0:
+        print("coding_Dojo!!")
+    else:
+        print(z)
+
+p=0
+for u in range (0,500000,1):
+    if u%2!=0:
+        p=p+u
+
+print(p)
+
+
+for m in range (2018, 0, -4):
+    print (m)
+
+
+lowNum=2
+highNum=9
+mult=3
