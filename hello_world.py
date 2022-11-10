0435669f57ce8d7dd23c7f0b0a85bb92bd092ee7
Mohammed.Abdelhafiz <mg.abdelhafiz@gmail.com>
2022-11-10 20:57:52 +0300

    Python_First_Assignment

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Abdelhafiz"
+print( "Hello" , name  )	# with a comma
+print( "Hello " + name )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 28
+print( "Hello" , name )	# with a comma
+fave_food1 = "mansaf"
+fave_food2 = "karshat"
+print( "i love to eat {} " .format(fave_food1 + fave_food2)) # with .format()
+print(f"i love to eat {fave_food2} and {fave_food1}" ) # with an f string
+
